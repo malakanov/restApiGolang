41ddcdbc30ec45bae0e8e3002ffe861a9382341d
sergey.malakanov <sergey.malakanov@gmail.com>
2019-12-11 13:51:58 +0500

    add router

--- main.go
+++ main.go
@@ -2,28 +2,60 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	_ "fmt"
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
 )
 
-type Book struct{
-	ID int`json:id`
-	Title string `json:title`
+type Book struct {
+	ID     string `json:id`
+	Title  string `json:title`
 	Author string `json:author`
-	Year string `json: year`
+	Year   string `json: year`
 }
 
 var books []Book
 
 func main() {
-	router := mux.NewRouter() 
+	router := mux.NewRouter()
+
+	books = append(books,
+		Book{ID: "1", Title: "Golang pointers", Author: "Mr. Golang", Year: "2010"},
+		Book{ID: "2", Title: "Goroutines", Author: "Mr. Goroutine", Year: "2011"},
+		Book{ID: "3", Title: "Golang routers", Author: "Mr. Router", Year: "2012"},
+		Book{ID: "4", Title: "Golang concurrency", Author: "Mr. Currency", Year: "2013"},
+		Book{ID: "5", Title: "Golang good parts", Author: "Mr. Good", Year: "2014"})
 
 	router.HandleFunc("/books", getBooks).Methods("GET")
 	router.HandleFunc("/books/{id}", getBook).Methods("GET")
-	router.HandleFunc("/books/{id}", addBook).Methods("POST")
-	router.HandleFunc("/books/{id}", updateBook).Methods("PUT")
+	router.HandleFunc("/books", addBook).Methods("POST")
+	router.HandleFunc("/books", updateBook).Methods("PUT")
+	router.HandleFunc("/books/{id}", removeBook).Methods("DELETE")
 
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(books)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, book := range books {
+		if book.ID == params["id"]{
+			json.NewEncoder(w).Encode(&book)
+		}
+	}
+}
+
+func addBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Add one book")
+}
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Update a book")
+}
+
+func removeBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Remove a book")
+}
